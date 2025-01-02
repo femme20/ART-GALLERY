81948715bbf661b1ef936d91510574c860a019b2
femme20 <thewaniarahman@gmail.com>
2025-01-02 13:00:06 +0500

    Update Art-Gallery.js

--- Art-Gallery.js
+++ Art-Gallery.js
@@ -1,9 +1,14 @@
-function total(input)
-{
+const btn = document.getElementById("add-to-cart-button");
+
+    let numItems = 0;
     let total = 0;
-    total += input;
 
-    document.getElementById("total").textContent = total;
+function addToCard(input)
+{
 
+    numItems++;
+    total += input
 
-}
+    alert(`Number of items: ${numItems}`);
+    alert(`Total: $${total}`);
+
